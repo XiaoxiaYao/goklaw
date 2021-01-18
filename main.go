46abb0925277bdbcbe5e1c9322297ab4d7fd6170
michaelyao-eb <yaoxiaoxia@easternbay.cn>
2021-01-18 14:37:17 +0800

    Get the city list.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
 func main() {
@@ -19,6 +20,15 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("%s\n", body)
+		printCityList(body)
 	}
+}
+
+func printCityList(contents []byte) {
+	result := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	matches := result.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
+	}
+	fmt.Printf("matches %d", len(matches))
 }
